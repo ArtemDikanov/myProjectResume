6623e612212685cae879355cd820497cd3de622c
ArtemDikanov <Tyuiu.DikanovAA@outlook.com>
2026-03-25 22:12:55 +0500

    Сделан адаптив about-block. Написан тестовый вариант скрипта для слайдера, который будет дорабатываться и использоваться в дальнейшем.

--- src/scripts/header-dialog.js
+++ src/scripts/header-dialog.js
@@ -1,16 +1,6 @@
 export function dialogOpen() {
     const dialog = document.querySelector('.header__dialog');
     const body = document.querySelector("body");
-    dialog.innerHTML = `
-        <nav class="header__menu dialog">
-          <ul class="header__menu-list dialog">
-            <li class="header__menu-item menu-link"><a href="" class="link dialog">About us</a></li>
-            <li class="header__menu-item menu-link"><a href="" class="link dialog">Destinations</a></li>
-            <li class="header__menu-item menu-link"><a href="" class="link dialog">Packages</a></li>
-            <li class="header__menu-item menu-link"><a href="" class="link dialog">Testimonial</a></li>
-            <li class="header__menu-item menu-link"><a href="" class="link dialog">Contact Us</a></li>
-          </ul>
-        </nav>`
     dialog.show();
     body.classList.add('body-no-scroll');
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,5 +6,11 @@ import { clickBurger } from './header-burger';
 
 clickBurger();
 
+/* import { sliderGallery } from './about-slider';
+
+if (window.matchMedia('(max-width: 800px)').matches) {
+    sliderGallery();
+} */
+
 
 
