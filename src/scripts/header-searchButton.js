55fa7357db2f7c28d47d934df3fd2245a0d7e995
ArtemDikanov <Tyuiu.DikanovAA@outlook.com>
2026-04-05 14:52:05 +0500

    Верстка и адаптив сайта закончены

--- src/scripts/header-searchButton.js
+++ src/scripts/header-searchButton.js
@@ -1,15 +1,15 @@
 export function viewInput() {
-    const searchButton = document.querySelector('.header__searchButton');
+    const searchButton = document.querySelector('.header__search-button');
     
     // Проверяем существование кнопки
     if (!searchButton) {
-        console.warn('Элемент .header__searchButton не найден');
+        console.warn('Элемент .header__search-button не найден');
         return;
     }
     
     searchButton.addEventListener('click', () => {
             const searchInput = document.querySelector('.header__input');
-            const itemInput = document.querySelector('.header__menu-itemInput');
+            const itemInput = document.querySelector('.header__menu-item-input');
             // Проверяем существование поля ввода
             if (searchInput && itemInput) {
                 searchInput.classList.remove('visually-hidden');
